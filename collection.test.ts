3608aaf6b3562fddc9fb110e870d84f6b0772fc5
hugoalh <hugoalh@gmail.com>
2025-02-17 17:19:31 +0800

    Update test

--- collection.test.ts
+++ collection.test.ts
@@ -1,25 +1,32 @@
 import { assertEquals } from "STD/assert/equals";
-import { sortCollectionByKeys } from "./collection.ts";
+import {
+	sortCollectionByKeys,
+	sortCollectionByValues
+} from "./collection.ts";
 const sample1 = {
 	d: "0bd85eab08e1",
 	b: "abdc6b784c27",
 	c: "a5e0a18f58f2",
 	a: "abee7ff65308"
 };
-Deno.test("Normal 1", { permissions: "none" }, () => {
+Deno.test("1 Key Normal", { permissions: "none" }, () => {
 	const result = Object.keys(sortCollectionByKeys(sample1));
 	assertEquals(result, ["a", "b", "c", "d"]);
 });
-Deno.test("Special 1", { permissions: "none" }, () => {
+Deno.test("1 Key Special", { permissions: "none" }, () => {
 	const result = Object.keys(sortCollectionByKeys(sample1, {
 		keysSpecial: ["c"]
 	}));
 	assertEquals(result, ["c", "a", "b", "d"]);
 });
-Deno.test("RestFirst Special 1", { permissions: "none" }, () => {
+Deno.test("1 Key RestFirst Special", { permissions: "none" }, () => {
 	const result = Object.keys(sortCollectionByKeys(sample1, {
 		keysSpecial: ["c"],
 		restPlaceFirst: true
 	}));
 	assertEquals(result, ["a", "b", "d", "c"]);
+});
+Deno.test("1 Value", { permissions: "none" }, () => {
+	const result = Object.values(sortCollectionByValues(sample1));
+	assertEquals(result, ["0bd85eab08e1", "a5e0a18f58f2", "abdc6b784c27", "abee7ff65308"]);
 });
